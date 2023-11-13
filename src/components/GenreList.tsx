64b61259f69f592dcb0d312c38df8f920b216a3a
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2023-11-13 18:04:27 +0330

    Refactor: fetch data with react-query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -35,7 +35,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
               <GenreListSkeleton />
             </ListItem>
           ))}
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image

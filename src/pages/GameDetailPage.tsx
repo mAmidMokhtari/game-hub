b479d07cf205d34c1d44a8ee7454a2b661c1e5d5
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2023-11-19 19:38:36 +0330

    Build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { Heading, Spinner } from "@chakra-ui/react";
 
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
@@ -17,6 +18,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
